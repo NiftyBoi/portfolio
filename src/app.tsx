6f907f8617d8158a00295932140408b505fa5c21
lovnelies <lovnelies@gmail.com>
2025-04-14 17:43:27 -0400

    estructuración

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="bg-white text-gray-800">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section className="flex items-center justify-center h-screen bg-gradient-to-br from-gray-100 to-gray-300">
+      <h1 className="text-6xl md:text-8xl font-bold text-gray-800 animate-pulse">
+        (x﹏x)
+      </h1>
+    </section>
+  );
+};
+
+export default Home;
